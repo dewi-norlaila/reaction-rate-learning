4c163c15d8c6470f3eeb27edb5027f16db32765e
dewi-norlaila <dewinorlaila5@gmail.com>
2025-12-12 21:23:12 +0700

    Update homepage and base path to correct GitHub Pages URL

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-   base: '/tes.github.io',
+   base: '/reaction-rate-learning/',
   plugins: [react()],
   server: {
     port: 3000,
